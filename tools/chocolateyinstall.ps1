839c855ccbc281bb06603a3548528e75933c8315
Matt Ronchetto <hello@doamatto.xyz>
2021-09-04 10:29:05 -0700

    chore: incremented version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url = 'https://github.com/vlang/v/releases/download/0.2.2/v_windows.zip'
+$url = 'https://github.com/vlang/v/releases/download/0.2.4/v_windows.zip'
 $checksum = '8DBF0766DE9124ECF1208731B7B244286A7CB857BDB49E95D4213651EE2D973A'
 
 $packageArgs = @{
